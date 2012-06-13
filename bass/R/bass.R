e570537b66d6698c3611eac6551a9c252867c8cc
Josh London <josh.london@noaa.gov>
2012-06-13 14:42:38 -0700

    INITIAL roxygenize FOR PACKAGE

---
+++ bass/R/bass.R
@@ -0,0 +1,10 @@
+#' Tools and Analysis Functions for the Bering-Arctic Seal Surveys
+#'
+#' bass is a central package for creating basic/global functions and data
+#' related to the Bering-Arctic Seal Surveys. The 2012 BOSS surveys are the
+#' first surveys to rely on this package
+#'
+#' @docType package
+#' @name bass
+#' @aliases bass bass-package
+NULL
